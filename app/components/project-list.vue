7b4adf0231930612e243c1fa6faf0ac3d7c84913
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-01-12 00:51:32 +0100

    filter & sort

--- app/components/project-list.vue
+++ app/components/project-list.vue
@@ -5,7 +5,7 @@
     <section v-else-if="error">{{ error }}</section>
     <section v-else>
       <ul class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        <li v-for="repo in data" :key="repo.id" class="border border-gray-600 rounded-sm p-4 hover:bg-gray-400 font-mono">
+        <li v-for="repo in repos" :key="repo.id" class="border border-gray-600 rounded-sm p-4 hover:bg-gray-400 font-mono">
           <a :href="repo.html_url" target="_blank">
             <div class="flex justify-between text-sm">
               <div>{{ repo.name }}</div>
@@ -21,5 +21,11 @@
   </div>
 </template>
 <script setup>
-  const { error, pending, data} = await useFetch('https://api.github.com/users/mjkatgithub/repos')
+const { error, pending, data} = await useFetch(
+  'https://api.github.com/users/mjkatgithub/repos'
+)
+const repos = computed(
+  () => data.value.filter(repo => repo.description)
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+);
 </script>
