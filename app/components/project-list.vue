e47ac31230c28eaafb80bf3b45b54b213a999a4e
mjkatgithub <melchior.kannengiesser@gmx.de>
2025-01-11 05:47:47 +0100

    Add project List

---
+++ app/components/project-list.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="mb-10">Take a look at my GitHub projects!</p>
+    <section v-if="pending">Loading ...</section>
+    <section v-else-if="error">{{ error }}</section>
+    <section v-else>
+      <ul class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <li v-for="repo in data" :key="repo.id" class="border border-gray-600 rounded-sm p-4 hover:bg-gray-400 font-mono">
+          <a :href="repo.html_url" target="_blank">
+            <div class="flex justify-between text-sm">
+              <div>{{ repo.name }}</div>
+              <div>{{ repo.stargazers_count }} ★</div>
+            </div>
+            <p class="text-sm text-gray-700">
+              {{ repo.description }}
+            </p>
+          </a>
+        </li>
+      </ul>
+      </section>
+  </div>
+</template>
+<script setup>
+  const { error, pending, data} = await useFetch('https://api.github.com/users/mjkatgithub/repos')
+</script>
---
+++ app/pages/projects.vue
@@ -0,0 +1,6 @@
+<template>
+  <article>
+    <h1 class="text text-4xl font-extrabold font-sans text-gray-900 mb-10">Projects</h1>
+    <project-list />
+  </article>
+</template>
